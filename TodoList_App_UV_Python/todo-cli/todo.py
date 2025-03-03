7f32a176843adec23e3f548197aa250a0202a2f0
Fatima367 <princess.fatima3275@gmail.com>
2025-03-03 16:19:27 +0500

    Ramadan-Coding-Nights

---
+++ TodoList_App_UV_Python/todo-cli/todo.py
@@ -0,0 +1,15 @@
+import click # a library to create cli
+import json # to save and load tasks from a file
+import os # to check if the file exists
+
+TODO_FILE = "todo.json"
+
+def load_tasks():
+    if os.path.exists(TODO_FILE):
+        return []
+    with open(TODO_FILE, "r") as file:
+        return json.load(file)
+    
+def save_tasks(tasks):
+    with open(TODO_FILE, "w") as file:
+        json.dump(tasks, file, indent=4)
