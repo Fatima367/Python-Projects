58bcc42d44eaaf7b7bdba7adf37f30131f8cde25
Fatima367 <princess.fatima3275@gmail.com>
2025-03-03 20:32:12 +0500

    Ramadan-Coding-Nights

--- TodoList_App_UV_Python/todo-cli/todo.py
+++ TodoList_App_UV_Python/todo-cli/todo.py
@@ -25,15 +25,14 @@ def cli():
 def add(task):
     """Add a new task to the list"""
     tasks = load_tasks()
-    tasks.append({"task" : task, "Done": False})
+    tasks.append({"task" : task, "done": False})
     save_tasks(tasks)
-    click.echo(f"Task added successfully: {task}")
+    click.echo(f"Task added: {task}")
 
 @click.command()
 def list():
     """List all tasks"""
     tasks = load_tasks()
-
     if not tasks:
         click.echo("No tasks found..")
         return
@@ -42,12 +41,34 @@ def list():
         click.echo(f"{index}. {task["task"]} [{status}]")
 
 @click.command()
+@click.argument("task_number", type=int)
 def complete(task_number):
     """"Mark a task as completed"""
     tasks = load_tasks()
+    if 0 < task_number <= len(tasks):
+        tasks[task_number - 1]["done"] = True
+        save_tasks(tasks)
+        click.echo(f"Task {task_number} marked as completd!")
+    else:
+        click.echo(f"Invalid task number {task_number}.")
+
+
+@click.command()
+@click.argument("task_number", type=int)
+def remove(task_number):
+    """Delete task from list"""
+    tasks = load_tasks()
+    if 0 < task_number <= len(tasks):
+        removed_task = tasks.pop(task_number - 1)
+        save_tasks(tasks)
+        click.echo(f"Removed task: {removed_task['task']}")
+    else:
+        click.echo(f"Invalid task number {task_number}")
 
 cli.add_command(add)
 cli.add_command(list)
+cli.add_command(complete)
+cli.add_command(remove)
 
 if __name__ == "__main__":
     cli()
