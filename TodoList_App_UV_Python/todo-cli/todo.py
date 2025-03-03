a16d3cfcaf3cf4ca2a78c8b9d3dc3dbde5687769
Fatima367 <princess.fatima3275@gmail.com>
2025-03-03 19:52:54 +0500

    Ramadan-Coding-Nights

--- TodoList_App_UV_Python/todo-cli/todo.py
+++ TodoList_App_UV_Python/todo-cli/todo.py
@@ -13,3 +13,41 @@ def load_tasks():
 def save_tasks(tasks):
     with open(TODO_FILE, "w") as file:
         json.dump(tasks, file, indent=4)
+
+
+@click.group()
+def cli():
+    """Simple Todo List Manager"""
+    pass
+
+@click.command()
+@click.argument("task")
+def add(task):
+    """Add a new task to the list"""
+    tasks = load_tasks()
+    tasks.append({"task" : task, "Done": False})
+    save_tasks(tasks)
+    click.echo(f"Task added successfully: {task}")
+
+@click.command()
+def list():
+    """List all tasks"""
+    tasks = load_tasks()
+
+    if not tasks:
+        click.echo("No tasks found..")
+        return
+    for index, task in enumerate(tasks, 1):
+        status = "✅" if task["done"] else "❌"
+        click.echo(f"{index}. {task["task"]} [{status}]")
+
+@click.command()
+def complete(task_number):
+    """"Mark a task as completed"""
+    tasks = load_tasks()
+
+cli.add_command(add)
+cli.add_command(list)
+
+if __name__ == "__main__":
+    cli()
