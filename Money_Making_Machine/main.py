4722f0f3813b4cab419af4517643d67d8528576f
Fatima367 <princess.fatima3275@gmail.com>
2025-03-08 16:48:20 +0500

    MMM with uv & python

--- Money_Making_Machine/main.py
+++ Money_Making_Machine/main.py
@@ -1,6 +1,49 @@
-def main():
-    print("Hello from money-making-machine!")
+import streamlit as st
+import random
+import time
+import requests # helps us in calling APIs
 
+st.title("Money Making Machine")
 
-if __name__ == "__main__":
-    main()
+def generate_money():
+    return random.randint(1, 1000)
+
+st.subheader("Instant Cash Generator")
+
+if st.button("Generate Money"):
+    st.write("Counting your money...")
+    time.sleep(2)
+    amount = generate_money()
+    st.success(f"You've made ${amount}!")
+
+def fetch_side_hustle():
+    try:
+        response = requests.get("http://127.0.0.1:8000/side_hustles?apiKey=123456789")
+        if response.status_code == 200:
+            hustles = response.json()
+            return  hustles["side_hustle"]
+        else:
+            return("Freelancing")
+    except:
+        return("Something went wrong")
+    
+st.subheader("Side Hustle Ideas")
+if st.button("Generate Hustle"):
+    idea = fetch_side_hustle()
+    st.success(idea)
+
+def fetch_money_quotes():
+    try:
+        response = requests.get("http://127.0.0.1:8000/money_quotes")
+        if response.status_code == 200:
+            quote = response.json()
+            return quote["money_quote"]
+        else:
+            return ("Make money!!")
+    except:
+        return ("Something went wrong!")
+    
+st.subheader("Money-Making Motivations")
+if st.button("Get Motivation"):
+    quote = fetch_money_quotes()
+    st.info(quote)
