181995fc136af365a6a75f271014277c4677e1ed
Fatima367 <princess.fatima3275@gmail.com>
2025-03-12 21:24:55 +0500

    Ramadan-Coding-Nights

--- 08_Quiz_App/main.py
+++ 08_Quiz_App/main.py
@@ -1,6 +1,61 @@
-def main():
-    print("Hello from 08-quiz-app!")
+import streamlit as st
+import random
+import time
 
 
-if __name__ == "__main__":
-    main()
+st.title("📝Quiz Appilication")
+
+questions = [
+    {
+        "question": "What is the capital of Pakistan?",
+        "options": ["Lahore", "Karachi", "Islamabad", "Peshawar"],
+        "answer": "Islamabad",
+    },
+    {
+        "question": "Who is the founder of Pakistan?",
+        "options": [
+            "Allama Iqbal",
+            "Liaquat Ali Khan",
+            "Muhammad Ali Jinnah",
+            "Benazir Bhutto",
+        ],
+        "answer": "Muhammad Ali Jinnah",
+    },
+    {
+        "question": "Which is the national language of Pakistan?",
+        "options": ["Punjabi", "Urdu", "Sindhi", "Pashto"],
+        "answer": "Urdu",
+    },
+    {
+        "question": "What is the currency of Pakistan?",
+        "options": ["Rupee", "Dollar", "Taka", "Riyal"],
+        "answer": "Rupee",
+    },
+    {
+        "question": "Which city is known as the City of Lights in Pakistan?",
+        "options": ["Lahore", "Islamabad", "Faisalabad", "Karachi"],
+        "answer": "Karachi",
+    },
+]
+
+if "current_questions" not in st.session_state:
+    st.session_state.current_questions = random.choice(questions)
+
+question = st.session_state.current_questions
+
+st.subheader(question["question"])
+
+selected_option = st.radio("Choose your answer", question["options"], key="answer")
+
+if st.button("Submit Answer"):
+    if selected_option == question["answer"]:
+        st.success("✅Correct!")
+        st.balloons()
+    else:
+        st.error("❌Incorrect! The correct answer is " + question["answer"])
+    
+    time.sleep(2)
+
+    st.session_state.current_questions = random.choice(questions)
+
+    st.rerun()
